c88ba04f3acc6a7c65b77a923f1333d56e6ba9d0
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-23 12:35:47 -0400

    FIRST COMMIT

---
+++ src/main/java/br/com/desafiogrupoallcross/DesafioGrupoAllcrossApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiogrupoallcross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGrupoAllcrossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGrupoAllcrossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafiogrupoallcross/DesafioGrupoAllcrossApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiogrupoallcross;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGrupoAllcrossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
