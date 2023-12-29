e3f6d2ffe14e533619c2b86f83a83bb1fcc2104b
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-29 11:04:35 -0400

    ...

--- src/main/java/br/com/desafiogrupoallcross/config/security/utils/UtilImpl.java
+++ src/main/java/br/com/desafiogrupoallcross/config/security/utils/UtilImpl.java
@@ -14,7 +14,7 @@ public class UtilImpl implements Util {
                 .id(usuarioBusiness.getId())
                 .username(usuarioBusiness.getUsername())
                 .password(usuarioBusiness.getPassword())
-                .role(usuarioBusiness.getRole()ar)
+                .role(usuarioBusiness.getRole())
                 .build();
     }
 
