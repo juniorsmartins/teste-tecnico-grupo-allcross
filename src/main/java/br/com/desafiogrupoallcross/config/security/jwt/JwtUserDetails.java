a541be11f5dfc7e1a975f6ac42360536c1ae8010
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-29 13:37:45 -0400

    FEAT: add JwtUserDetails

---
+++ src/main/java/br/com/desafiogrupoallcross/config/security/jwt/JwtUserDetails.java
@@ -0,0 +1,25 @@
+package br.com.desafiogrupoallcross.config.security.jwt;
+
+import br.com.desafiogrupoallcross.config.security.entity.UsuarioEntity;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+// Classe responsável por criar um objeto para armazenar informações do usuário logado
+public class JwtUserDetails extends User {
+
+    private UsuarioEntity usuarioEntity;
+
+    public JwtUserDetails(UsuarioEntity usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(usuario.getRole().name()));
+        this.usuarioEntity = usuario;
+    }
+
+    public Long getId() {
+        return this.usuarioEntity.getId();
+    }
+
+    public String getRole() {
+        return this.usuarioEntity.getRole().name();
+    }
+}
+
