100baff6029b920978fadee8ec967e2be9e4a6f3
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-02 18:53:09 -0400

    ...

--- src/main/java/br/com/desafiogrupoallcross/application/core/usecase/JasperReportUseCase.java
+++ src/main/java/br/com/desafiogrupoallcross/application/core/usecase/JasperReportUseCase.java
@@ -23,13 +23,13 @@ public class JasperReportUseCase implements JasperReportInputPort {
 
     private static final Logger log = LoggerFactory.getLogger(JasperReportUseCase.class);
 
-    private static final String JASPER_DIRETORIO = "classpath:jasper_reports/";
+    private static final String JASPER_DIRETORIO = "classpath:jasper/";
 
     private final ResourceLoader resourceLoader;
 
     private final DataSource dataSource;
 
-    private Map<String, Object> parametros = new HashMap<>();
+    private Map<String, Object> params = new HashMap<>();
 
     public JasperReportUseCase(ResourceLoader resourceLoader,
                                DataSource dataSource) {
@@ -39,7 +39,8 @@ public class JasperReportUseCase implements JasperReportInputPort {
 
     @Override
     public void addParams() {
-        this.parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+        this.params.put("REPORT_LOCALE", new Locale("pt", "BR"));
+        this.params.put("IMAGEM_DIRETORIO", JASPER_DIRETORIO);
     }
 
     @Override
@@ -51,7 +52,7 @@ public class JasperReportUseCase implements JasperReportInputPort {
         try {
             Resource resource = resourceLoader.getResource(JASPER_DIRETORIO.concat(nomeRelatorio));
             InputStream stream = resource.getInputStream();
-            JasperPrint jasperPrint = JasperFillManager.fillReport(stream, parametros, dataSource.getConnection());
+            JasperPrint jasperPrint = JasperFillManager.fillReport(stream, params, dataSource.getConnection());
             bytes = JasperExportManager.exportReportToPdf(jasperPrint);
 
         } catch (IOException | JRException | SQLException ex) {
