848d2cd30e004f6b1b9cdbc332a850a93d012b7b
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-01-02 16:42:57 -0400

    FEAT: add exception para JasperReports

---
+++ src/main/java/br/com/desafiogrupoallcross/config/exception/http_500/JasperReportUseCaseException.java
@@ -0,0 +1,13 @@
+package br.com.desafiogrupoallcross.config.exception.http_500;
+
+public final class JasperReportUseCaseException extends ProblemaInternoNoServidorException {
+
+    public JasperReportUseCaseException(String mensagem) {
+        super(mensagem);
+    }
+
+    public JasperReportUseCaseException() {
+        this("Falha ao gerar PDF de Relatório com JasperReport.");
+    }
+}
+
